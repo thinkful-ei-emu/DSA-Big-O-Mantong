eec3ac98b604581d3bea9d9df0b7be3d42bdcc13
Mantong Zhao <zhaomantong0203@gmail.com>
2019-08-27 09:45:46 -0700

    bigO

---
+++ bigO.js
@@ -0,0 +1,97 @@
+//1. What is the Big O for this?
+//1) O(1) constant ?
+//2) O(n) 15 It's linear algorithm 
+
+//2 Even or odd
+function isEven(value) {
+    if (value % 2 == 0) {
+        return true;
+    }
+    else
+        return false;
+    }
+};
+
+//O(1) It's just constant. No matter size of input, the algorithm take same time to compelet.
+
+//3. Are you here?
+function areYouHere(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        const el1 = arr1[i];
+        for (let j = 0; j < arr2.length; j++) {
+            const el2 = arr2[j];
+            if (el1 === el2) return true;
+        }
+    }
+    return false;
+}
+//O(n^k)--O(n^2) It's a nested loop.
+//4.  Doubler
+function doubleArrayValues(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] *= 2;
+    }
+    return array;
+}
+//O(n) linear , runtime propotion of input 
+//Naive search
+function naiveSearch(array, item) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
+}
+//O(n) linear , runtime propotion of input 
+
+//6. Creating pairs:
+function createPairs(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for(let j = i + 1; j < arr.length; j++) {
+            console.log(arr[i] + ", " +  arr[j] );
+        }
+    }
+}
+//O(n^k) nested 
+//7. Compute the sequence
+function compute(num) {
+    let result = [];
+    for (let i = 1; i <= num; i++) {
+
+        if (i === 1) {
+            result.push(0);
+        }
+        else if (i == 2) {
+            result.push(1);
+        }
+        else {
+            result.push(result[i - 2] + result[i - 3]);
+        }
+    }
+    return result;
+}
+//O(n) Fibbonanci function generates an array of that Fibbonanci sequence. 
+//8. An efficient search
+function efficientSearch(array, item) {
+    let minIndex = 0;
+    let maxIndex = array.length - 1;
+    let currentIndex;
+    let currentElement;
+
+    while (minIndex <= maxIndex) {
+        currentIndex = Math.floor((minIndex + maxIndex) / 2);
+        currentElement = array[currentIndex];
+
+        if (currentElement < item) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentElement > item) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+
